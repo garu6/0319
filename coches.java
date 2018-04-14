afaff699c4bb38cc4fa75851fe24143f09500cdd
Edgar Alvarez <edgarfabero85@gmail.com>
2018-04-14 02:48:04 +0200

    creado el constructor con los atributos

---
+++ coches.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class coches here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class coches
+{
+    // instance variables - replace the example below with your own
+    private String marca;
+    private String modelo;
+    private String color;
+    private int añoFabricacion;
+    private int kilometros;
+    private int potencia;
+
+    /**
+     * Constructor for objects of class coches
+     */
+    public coches( String marca, String modelo, String color, int añoFabricacion , int potencia )
+    {
+        kilometros =0;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.añoFabricacion = añoFabricacion;
+        this.potencia = potencia;
+    }
+}
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
